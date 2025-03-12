4729b4a47743fb6924696a8d7267a5566e3db0c4
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-03-12 12:08:27 +0530

    Change nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Clock In-Out',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'clock.png' },
+      ]
+    }
+  }
 })
