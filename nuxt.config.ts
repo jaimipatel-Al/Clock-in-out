62d1544a4cbe1dd0f2d0bb4318c02f4458030761
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-03-12 13:30:38 +0530

    Set up tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   app: {
     head: {
-      title: 'Clock In-Out',
+      title: "Clock In-Out",
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: 'clock.png' },
-      ]
-    }
-  }
-})
+      link: [{ rel: "icon", type: "image/x-icon", href: "clock.png" }],
+    },
+  },
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  tailwindcss: {
+    cssPath: [`/assets/css/tailwind.css`, { injectPosition: "first" }],
+    config: {},
+    configPath: "tailwind.config.js",
+    viewer: true,
+    exposeConfig: false,
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
